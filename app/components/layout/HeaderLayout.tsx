e7b6daba46e127f3276eedff12aeed96b6ea2a4d
noel <admin@agenciaservitec.com>
2024-12-16 14:57:03 -0500

    Update HeaderLayout

--- app/components/layout/HeaderLayout.tsx
+++ app/components/layout/HeaderLayout.tsx
@@ -1,21 +1,34 @@
 import React from "react";
+import Link from "next/link";
 
 export const HeaderLayout = (): React.ReactNode => {
   return (
     <header className="bg-background p-6 w-f bg-blue-950 text-cyan-50 flex justify-between align-center">
       <div className="logo grid place-items-center">
-        <h1>Mi Encabezado</h1>
+        <Link href="/">
+          <img
+            src="https://servitecperu.com/logotipo-soft.png"
+            alt="logo servicont"
+            className="w-full max-w-[170px]"
+          />
+        </Link>
       </div>
       <nav className="w-auto justify-between items-center px-5 py-2">
         <ul className="w-full flex justify-center gap-5">
           <li>
-            <a href="/public">Inicio</a>
+            <Link href="/">Inicio</Link>
           </li>
           <li>
-            <a href="/about">Acerca</a>
+            <Link href="/about-us">Sobre nosotros</Link>
           </li>
           <li>
-            <a href="/contact">Contacto</a>
+            <Link href="/services">Servicios</Link>
+          </li>
+          <li>
+            <Link href="/plans">Planes</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contacto</Link>
           </li>
         </ul>
       </nav>
---
+++ app/plans/page.tsx
@@ -0,0 +1,7 @@
+export default function Plans() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-3">NUESTROS PLANES</h1>
+    </div>
+  );
+}
