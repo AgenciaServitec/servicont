21f9878a714b039cb9be434a151a1b5354dec0de
Angel Gala <galafloresangelemilio@gmail.com>
2024-12-17 16:43:58 -0500

    feature: add drawer, refactored header and refactored footer

--- app/components/layout/FooterLayout.tsx
+++ app/components/layout/FooterLayout.tsx
@@ -2,39 +2,56 @@ import React from "react";
 import {
   Facebook,
   Instagram,
-  LucideMail,
+  MailOpen,
   MapPin,
   MessageCircle,
-  Phone,
+  PhoneCall,
 } from "lucide-react";
 import { ContactInfo } from "@/app/components";
 import { SocialIcon } from "@/app/components/ui/SocialIcon";
 
 export const FooterLayout = (): React.ReactNode => {
   return (
-    <div className="bg-gray-100 pt-10">
-      <div className="flex flex-col md:flex-row lg:justify-around gap-8 p-6">
-        <div className="md:flex-1 lg:flex-none">
-          <h2 className="text-center lg:text-left uppercase font-bold mb-4">
+    <div className="pt-10">
+      <div className="bg-gray-100 flex flex-col md:flex-row xl:justify-evenly gap-8 px-3 py-10">
+        <div className="flex-1 xl:flex-none">
+          <h2 className="text-center md:text-left text-secondary uppercase font-bold mb-4">
             Contáctanos
           </h2>
-          <div className="flex flex-col gap-2">
-            <ContactInfo title="Celular" value="+51 987654321">
-              <Phone size={35} className="mb-4 lg:m-0 text-primary" />
-            </ContactInfo>
-            <ContactInfo
-              title="Correo electrónico"
-              value="contacto@servicont.com"
-            >
-              <LucideMail size={35} className="mb-4 lg:m-0 text-primary" />
-            </ContactInfo>
-            <ContactInfo
-              title="Dirección de la oficina"
-              value="Calle Vargas 179"
-            >
-              <MapPin size={35} className="mb-4 lg:m-0 text-primary" />
-            </ContactInfo>
-            <div className="flex justify-center items-center gap-3 lg:justify-start">
+          <div>
+            <div className="flex flex-col md:items-start gap-2 md:gap-6">
+              <ContactInfo title="Celular" value="+51 987654321">
+                <span className="flex md:hidden">
+                  <PhoneCall size={28} className="text-primary" />
+                </span>
+                <span className="hidden md:flex">
+                  <PhoneCall size={40} className="text-primary" />
+                </span>
+              </ContactInfo>
+              <ContactInfo
+                title="Correo electrónico"
+                value="contacto@servicont.com"
+              >
+                <span className="flex md:hidden">
+                  <MailOpen size={28} className="text-primary" />
+                </span>
+                <span className="hidden md:flex">
+                  <MailOpen size={40} className="text-primary" />
+                </span>
+              </ContactInfo>
+              <ContactInfo
+                title="Dirección de la oficina"
+                value="Calle Vargas 179"
+              >
+                <span className="flex md:hidden">
+                  <MapPin size={28} className="text-primary" />
+                </span>
+                <span className="hidden md:flex">
+                  <MapPin size={40} className="text-primary" />
+                </span>
+              </ContactInfo>
+            </div>
+            <div className="flex justify-center items-center gap-3 md:justify-start mt-8">
               <SocialIcon>
                 <Facebook size={20} className="text-white" />
               </SocialIcon>
@@ -44,13 +61,17 @@ export const FooterLayout = (): React.ReactNode => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-1 lg:flex-none items-center gap-4 text-center">
+        <div className="flex flex-col flex-1 xl:flex-none items-center gap-4 text-center">
           <h2 className="text-3xl font-bold">Conversa con un contador</h2>
           <span className="text-gray-500">Es hora de trabajar juntos</span>
-          <button className="flex justify-center items-center gap-2 py-2 px-6 bg-green-400 rounded-md text-white font-bold">
-            <MessageCircle size={30} />
+          <a
+            href="https://api.whatsapp.com/send/?phone=51900959965"
+            target="_blank"
+            className="no-underline flex justify-center items-center gap-2 py-2 px-6 bg-green-400 rounded-md text-white font-bold"
+          >
+            <MessageCircle size={28} />
             <span>Envíanos un whatsApp</span>
-          </button>
+          </a>
         </div>
       </div>
       <div className="text-xs text-gray-500 text-center p-6">
