122ff2e5b90c7db8adbf644ea05963f348160c76
PabloAquiseLozano <aquise.pablo@iestpgildaballivian.edu.pe>
2024-12-16 19:46:44 -0500

    add state for ServiceComponent

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 interface Props {
   className?: string;
+  style?: CSSProperties;
   children: React.ReactNode;
+  onClick?: () => void;
 }
 
-export const Button = ({ className, children }: Props): React.ReactNode => {
-  return <button className={className}>{children}</button>;
+export const Button = ({
+  className,
+  style,
+  children,
+  onClick,
+}: Props): React.ReactNode => {
+  return (
+    <button className={className} style={style} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
