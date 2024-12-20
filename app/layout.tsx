779b824f8523cce496e89da62d094165107ccdf6
Angel Gala <galafloresangelemilio@gmail.com>
2024-12-19 19:19:09 -0500

    Add: added payment page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import React from "react";
 import { HeaderLayout } from "@/components/layout/HeaderLayout";
 import { FooterLayout } from "@/components/layout/FooterLayout";
+import Head from "next/head";
 
 const noto = Noto_Sans_Display({ subsets: ["latin"] });
 
@@ -19,6 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="miendpoint/static/js/krypton-client/V4.0/ext/neon-reset.css"
+        />
+      </Head>
       <body className={`${noto.className}`}>
         <HeaderLayout />
         <main className="content min-h-[80svh]">{children}</main>
